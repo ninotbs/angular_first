988f81890cc66b3e7bfe2ebdefb0abe5669d9b64
ninotbs <nino_tbs@mac.com>
2024-01-20 23:12:42 +0100

    update style.css file

--- first-angular-app/src/app/housing-location/housing-location.component.ts
+++ first-angular-app/src/app/housing-location/housing-location.component.ts
@@ -10,9 +10,11 @@ import { HousingLocation } from '../housinglocation'
     CommonModule
   ],
   template: `
-    <p>
-      housing-location works!
-    </p>
+  <section class="listing">
+    <img class="listing-photo" [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}">
+    <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+    <p class="listing-location">{{ housingLocation.city}}, {{housingLocation.state }}</p>
+  </section>
   `,
   styleUrl: './housing-location.component.css'
 })
