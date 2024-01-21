c1aa467a9d012c48e55ec034b349fdf8326c9087
ninotbs <nino_tbs@mac.com>
2024-01-21 23:27:23 +0100

    Integrate angular forms

--- first-angular-app/src/app/details/details.component.ts
+++ first-angular-app/src/app/details/details.component.ts
@@ -3,40 +3,40 @@ import { CommonModule } from '@angular/common'
 import { ActivatedRoute } from '@angular/router'
 import { HousingService } from '../housing.service'
 import { HousingLocation } from '../housinglocation'
+import { ReactiveFormsModule, FormGroup, FormControl } from '@angular/forms'
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [],
-  template: `
-    <article>
-      <img class="listing-photo" [src]="housingLocation?.photo"
-        alt="Exterior photo of {{housingLocation?.name}}"/>
-      <section class="listing-description">
-        <h2 class="listing-heading">{{housingLocation?.name}}</h2>
-        <p class="listing-location">{{housingLocation?.city}}, {{housingLocation?.state}}</p>
-      </section>
-      <section class="listing-features">
-        <h2 class="section-heading">About this housing location</h2>
-        <ul>
-          <li>Units available: {{housingLocation?.availableUnits}}</li>
-          <li>Does this location have wifi: {{housingLocation?.wifi}}</li>
-          <li>Does this location have laundry: {{housingLocation?.laundry}}</li>
-        </ul>
-      </section>
-    </article>
-  `,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
+  template: './details.component.html',
   styleUrl: './details.component.css'
 })
   
 export class DetailsComponent {
-
   route: ActivatedRoute = inject(ActivatedRoute)
   housingService = inject(HousingService)
   housingLocation: HousingLocation | undefined
+  
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+  })
 
   constructor() {
     const housingLocationId = Number(this.route.snapshot.params['id'])
     this.housingLocation = this.housingService.getHouseingLocationById(housingLocationId)
+  }
+
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? ''
+    )
   }
 }
