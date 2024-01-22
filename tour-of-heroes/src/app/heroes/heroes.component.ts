e008a375bc7ebf8c0b2b49ed28274a25a32c2164
ninotbs <nino_tbs@mac.com>
2024-01-22 23:08:26 +0100

    Display a list

--- tour-of-heroes/src/app/heroes/heroes.component.ts
+++ tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,18 +1,28 @@
 import { Component } from '@angular/core'
+import { NgFor, NgIf, UpperCasePipe } from '@angular/common'
+import { FormsModule } from '@angular/forms'
 
 import { Hero } from '../hero'
+import { HEROES } from '../mock-heroes'
 
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [],
+  imports: [
+    NgFor,
+    NgIf,
+    UpperCasePipe,
+    FormsModule
+  ],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
   
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
+  heroes = HEROES
+  selectedHero?: Hero
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero
   }
 }
