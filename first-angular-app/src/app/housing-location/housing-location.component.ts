fdb86723fb493fbc5b240ccaccb0aad476b12959
ninotbs <nino_tbs@mac.com>
2024-01-22 12:37:37 +0100

    update housing-location component

--- first-angular-app/src/app/housing-location/housing-location.component.ts
+++ first-angular-app/src/app/housing-location/housing-location.component.ts
@@ -2,12 +2,14 @@ import { Component, Input } from '@angular/core'
 import { CommonModule } from '@angular/common'
 
 import { HousingLocation } from '../housinglocation'
+import { RouterModule } from '@angular/router'
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   template: './housing-location.component.html',
   styleUrl: './housing-location.component.css'
