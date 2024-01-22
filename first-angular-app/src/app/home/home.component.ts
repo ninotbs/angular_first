e8c71d676df66c3710495b655ff591e195ebf207
ninotbs <nino_tbs@mac.com>
2024-01-22 12:43:30 +0100

    update home component

--- first-angular-app/src/app/home/home.component.ts
+++ first-angular-app/src/app/home/home.component.ts
@@ -25,4 +25,14 @@ export class HomeComponent {
     this.housingLocationList = this.housingService.getAllHousingLocations()
     this.filteredLocationList = this.housingLocationList
   }
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList
+      return
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter(
+      housingLocation => housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+    )
+  }
 }
